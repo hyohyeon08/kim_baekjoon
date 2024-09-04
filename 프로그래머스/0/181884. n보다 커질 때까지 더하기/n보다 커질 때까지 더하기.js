5e14fc1e4400f44c0e0f7149e6d1db84c18bcd46
hyohyeon08 <hyohyeonkim08@gmail.com>
2024-09-04 15:37:13 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,12 @@
+function solution(numbers, n) {
+    var answer = 0;
+    for(let i = 0; i < numbers.length; i++){
+        if(answer <= n){
+            answer += numbers[i];
+        }
+        else{
+            break
+        }
+    }
+    return answer;
+}

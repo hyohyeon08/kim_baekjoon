0bab53f471df6cfe168cd9b55d41cc4b9fa3da21
hyohyeon08 <hyohyeonkim08@gmail.com>
2024-11-05 15:00:59 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 1.98 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = 0
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if(arr[i][j] == arr[j][i]):
+                answer = 1
+            else:
+                answer = 0
+                break
+    return answer

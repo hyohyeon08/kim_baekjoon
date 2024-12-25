d7932760abe35fe288ffc028aa9c4459aa1050a1
hyohyeon08 <hyohyeonkim08@gmail.com>
2024-12-26 01:44:47 +0900

    [level 2] Title: 올바른 괄호, Time: 20.30 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,10 +1,10 @@
 def solution(s):
     new_s = []
 
-    for i in range(len(s)):
-        new_s.append(s[i])
+    for i in s:
+        new_s.append(i)
         if(len(new_s) == 0 or len(new_s) == 1): pass
-        elif(s[i] ==')'):
+        elif(i ==')'):
             new_s.pop()
             new_s.pop()
     if(len(new_s) == 0): return True

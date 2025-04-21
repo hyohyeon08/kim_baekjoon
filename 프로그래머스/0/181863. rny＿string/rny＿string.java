48226732f0b2469002ea614c1715553dc714c8cc
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-04-21 13:25:59 +0900

    [level 0] Title: rny_string, Time: 0.07 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String rny_string) {
+        StringBuilder answer = new StringBuilder();
+        for (char c : rny_string.toCharArray()) {
+            if (c == 'm') {
+                answer.append("rn");
+            } else {
+                answer.append(c);
+            }
+        }
+        return answer.toString();
+    }
+}

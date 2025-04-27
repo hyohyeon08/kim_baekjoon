a636298d86f0491e04f1f4bb5f7669120e17adc6
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-04-27 13:31:01 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.36 ms, Memory: 90.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        String c = Integer.toString(a) + Integer.toString(b);
+        String d = Integer.toString(b) + Integer.toString(a);
+        if(Integer.parseInt(c) > Integer.parseInt(d)){
+            answer = Integer.parseInt(c);
+        }
+        else{
+            answer = Integer.parseInt(d);
+        }
+        return answer;
+    }
+}

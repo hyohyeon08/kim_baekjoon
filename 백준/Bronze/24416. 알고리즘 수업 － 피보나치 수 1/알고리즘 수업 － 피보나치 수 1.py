0b3369fde5a34a4e3e9cfc608501ffa1735e4317
hyohyeon08 <hyohyeonkim08@gmail.com>
2025-05-12 17:21:03 +0900

    [Bronze I] Title: 알고리즘 수업 - 피보나치 수 1, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
+++ 백준/Bronze/24416. 알고리즘 수업 － 피보나치 수 1/알고리즘 수업 － 피보나치 수 1.py
@@ -1,19 +1,12 @@
 n = int(input())
 
 def fib(n):
-    if(n == 1 or n == 2):
-        return 1
-    else:
-        return (fib(n-1) + fib(n-2))
-
-def fibonacci(n):
-    dp = [0]*(n+1)
-    dp[1] = 1
-    dp[2] = 1
-    cnt = 0
+    a, b = 1, 1
     for i in range(3, n+1):
-        cnt += 1
-        dp[i] = dp[i-1] + dp[i-2]
-    return cnt
+        a, b = b, a+b
+    return b
+        
+def fibonacci(n):
+    return n-2
 
 print(fib(n), fibonacci(n))
